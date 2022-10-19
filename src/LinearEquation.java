565d309413b026991aaca4eef82e7d515854c451
BT_3W2_24 <henryw48@nycstudents.net>
2022-10-19 12:15:43 -0400

    added code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+
+    /* instance variables */
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    double m = (y2-y1) / (x2-x1);
+
+    public LinearEquation(int x1, int y1, int x2, int y2)
+
+    public String equation() {
+        return "";
+    }
+
+    public String lineInfo() {
+        return "";
+    }
+
+    public String slope() {
+        String slope = LinearEquation.getSlope();
+        System.out.println(getSlope);
+    }
+
+    public String yIntercept() {
+        return "";
+    }
+
+    public String distance() {
+        return "";
+    }
+
+    public String coordinateForX() {
+        return "";
+    }
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+
+        System.out.println("Enter coordinate 1: ");
+        Scanner coordinate1 = new Scanner(System.in);
+        System.out.println("Enter coordinate 2: ");
+        Scanner coordinate2 = new Scanner(System.in);
+        System.out.println();
+
+        System.out.println("------Line Info------");
+        System.out.println("The two points are: ");
+        System.out.println("The equation of the line between the two points is: ");
+        System.out.println("The slope of this line is: ");
+        System.out.println("The y-intercept of the line is: ");
+        System.out.println("The distance between the two points is: ");
+        System.out.println();
+
+        System.out.println("Enter a value for x: ");
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("The point on the line is: ");
+
+
+
+
+    }
+
 }
