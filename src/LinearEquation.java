336d09f63aeabf9983bb777308253e025fe1bde3
Henry Wong <henryw48@nycstudents.net>
2022-11-01 22:04:03 -0400

    added code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,4 +1,5 @@
 public class LinearEquation {
+    LinearEquationRunner obj = new LinearEquationRunner();
 
     /* instance variables */
     private int x1;
@@ -6,33 +7,55 @@ public class LinearEquation {
     private int y1;
     private int y2;
 
-    double m = (y2-y1) / (x2-x1);
-
-    public LinearEquation(int x1, int y1, int x2, int y2)
-
-    public String equation() {
-        return "";
+    public LinearEquation() {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
+
+    public double distance() {
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        distance = Math.round(distance * 100) / 100;
+        return distance;
+    }
+
+    public double slope() {
+        int change_in_y = y2 - y1;
+        int change_in_x = x2 - x1;
+        double getSlope = (change_in_y / change_in_x);
+        getSlope = Math.round(getSlope * 100) / 100;
+        return getSlope;
+    }
+
+
+    public double yIntercept() {
+        double b = y1 - (slope() * x1);
+        b = Math.round(b * 100) / 100;
+        return b;
+    }
+
+
+    public String Equation() {
+        return "y = " + slope() + "x + " + yIntercept();
+    }
+
+
+    public String coordinateForX(double xValue) {
+        double y = (slope() * xValue) + yIntercept();
+        return "(xValue, " + y + ")";
+    }
+
 
     public String lineInfo() {
-        return "";
+        System.out.println("The equation of the line between the two points is: " + Equation());
+        System.out.println("The slope of this line is: " + slope());
+        System.out.println("The y-intercept of the line is: " + yIntercept());
+        System.out.println("The distance between the two points is: " + distance());
+        System.out.println();
+        return null;
     }
 
-    public String slope() {
-        String slope = LinearEquation.getSlope();
-        System.out.println(getSlope);
     }
 
-    public String yIntercept() {
-        return "";
-    }
 
-    public String distance() {
-        return "";
-    }
-
-    public String coordinateForX() {
-        return "";
-    }
-
-}
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner Scan = new Scanner(System.in);
         System.out.println("Welcome!");
-
         System.out.println("Enter coordinate 1: ");
-        Scanner coordinate1 = new Scanner(System.in);
+        String coordinate1 = Scan.nextLine();
+
+        Scanner Scans = new Scanner(System.in);
         System.out.println("Enter coordinate 2: ");
-        Scanner coordinate2 = new Scanner(System.in);
+        String coordinate2 = Scans.nextLine();
         System.out.println();
+        System.out.println("The two points are: " + coordinate1 + " and " + coordinate2 );
 
-        System.out.println("------Line Info------");
-        System.out.println("The two points are: ");
-        System.out.println("The equation of the line between the two points is: ");
-        System.out.println("The slope of this line is: ");
-        System.out.println("The y-intercept of the line is: ");
-        System.out.println("The distance between the two points is: ");
-        System.out.println();
+        LinearEquation linearEquation = new LinearEquation();
+        linearEquation.lineInfo();
 
-        System.out.println("Enter a value for x: ");
         Scanner x = new Scanner(System.in);
+        System.out.println("Enter a value for x: ");
+        String value = Scan.nextLine();
+        double xValue = Double.valueOf(value);
 
-        System.out.println("The point on the line is: ");
+        System.out.println();
+        System.out.println("The point on the line is: " + linearEquation.coordinateForX(xValue));
 
 
 
